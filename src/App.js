1faf3e2dac4570f8bb54d8e50e1fb0f9ee1cdaff
Skyblocking <andremoreno_z@hotmail.com>
2021-12-28 20:53:46 -0600

    Creating a simple skeleton for CouponsComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return(
+    <div className="App">
+      Hi
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CouponsComponent.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function CouponsComponent(){
+
+    return(
+        <div>
+            <div>
+                <button>New</button>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+            <div>
+                <table border="1">
+                    <tbody>
+                        <tr>
+                            <th>Coupon Code</th>
+                            <th>Discount Percentage</th>
+                            <th>Category</th>
+                            <th>Expiry Date</th>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+
+}
